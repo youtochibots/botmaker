5763d80c8c9751ef5e8a63b8213080382536965d
youtochibots <izendejass600@gmail.com>
2021-11-18 16:45:00 -0600

    add package apidata

---
+++ packages/apidata/README.md
@@ -0,0 +1,31 @@
+# BOT CREADOR Package
+
+The botcreador package contains modules which include checkings.
+
+## Modules
+
+### Crear un bot en github
+
+Call a url in an end point that create a bot in github.
+
+#### Usage
+
+```
+(en-US) "Create bot nuevobotnombre"
+(en-US) "Create new bot with name nuevobotnombre"
+
+(fr-FR) "Crear un nuevo bot con el nombre nuevobotnombre"
+...
+```
+
+### Agreagr un tema en un bot en Pizzaron
+
+Call a url in Pizzaron with bot name and the new tema.
+
+#### Usage
+
+```
+(en-US) "bot nuevobotname add new tema nombretema"
+
+```
+
---
+++ packages/apidata/creaapi.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import requests
+import os
+import utils
+from time import time
+from pytube import YouTube
+
+def run(string, entities):
+	"""Call a url to create a bot in github"""
+
+	# db = utils.db()['db']
+	# query = utils.db()['query']
+	# operations = utils.db()['operations']
+	# apikey = utils.config('api_key')
+	# playlistid = utils.config('playlist_id')
+	# https://developers.google.com/youtube/v3/docs/playlistItems/list
+	# url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=' + playlistid + '&key=' + apikey
+	nombrebot = ''
+	nombretema = ''
+	result = ''	
+		
+	for item in entities:
+		if item['entity'] == 'elbot':
+			nombrebot = item['sourceText'].lower()
+
+	for item in entities:
+		if item['entity'] == 'eltema':
+			nombretema = item['sourceText'].lower()
+
+	url = 'https://youtochipizarron.herokuapp.com/' + nombrebot + '_' + nombretema
+	
+	utils.output('inter', 'checking', utils.translate('checking',{
+		'website_name': url
+	}))
+	# call the url to create a github bot branch/repository
+	try:
+		r = utils.http('GET', url)
+
+		# In case there is a problem like wrong settings
+		#if 'error' in r.json():
+		#	error = r.json()['error']['errors'][0]
+		#	return utils.output('settings_error', 'settings_error', utils.translate('settings_errors', {
+		#		'reason': error['reason'],
+		#		'message': error['message']
+		#	}))
+
+
+	# 	items = r.json()['rooms']
+		result += utils.translate('list_element', {
+				'repository_url': url,
+				'repository_name': nombrebot + '_' + nombretema
+			}
+		)
+
+	except requests.exceptions.RequestException as e:
+		return utils.output('request_error', 'request_error', utils.translate('request_errors'))
+
+
+	# Will synchronize the content (because "end" type) if synchronization enabled
+	return utils.output('end', 'success', utils.translate('success', {
+	  'nuevobot': nombrebot,
+	  'nuevotema': nombretema,
+	  'result': result		
+	}))
